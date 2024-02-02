bdee6e7f1607f8994ed3bff8033a2f4fc6971c3a
forhad446 <forhadkkhan@gmail.com>
2024-02-02 17:15:22 +0600

    firebase installed and setup signinwithemail and password and also googlesignin

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import Route from './Routes/Route'
+import AuthProvider from './Provider/AuthProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div className='max-w-7xl mx-auto overflow-hidden'>
-      <RouterProvider router={Route} />
-    </div>
+    <AuthProvider>
+      <div className='max-w-7xl mx-auto overflow-hidden'>
+        <RouterProvider router={Route} />
+      </div>
+    </AuthProvider>
   </React.StrictMode>,
 )
