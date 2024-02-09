09ea9ea6f8bc7a643437ccc812b5a43bd0dbab73
forhad446 <forhadkkhan@gmail.com>
2024-02-09 16:29:05 +0600

    submission form done

--- src/Pages/Assignments/AssignmentDetails/AssignmentDetails.jsx
+++ src/Pages/Assignments/AssignmentDetails/AssignmentDetails.jsx
@@ -1,10 +1,13 @@
 import { Link, useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import PageTitle from "../../Shared/PageTitle/PageTitle";
 import { Helmet } from "react-helmet-async";
 import { FaTwitter, FaInstagramSquare, FaFacebook, FaYoutube } from "react-icons/fa";
 import { FaFilePdf } from "react-icons/fa6";
 import useAllAssignment from "../../../hooks/useAllAssignment";
+import axios from "axios";
+import Swal from "sweetalert2";
+import useAuth from "../../../hooks/useAuth";
 
 const AssignmentDetails = () => {
 
@@ -12,8 +15,31 @@ const AssignmentDetails = () => {
     // const [assignment, setAssignment] = useState([])
     const [openModal, setOpenModal] = useState(false);
     const assignment = useAllAssignment()
+    const { user } = useAuth()
 
     const isExit = assignment.find(item => item?._id === id);
+
+    const handleSubmitAssignment = e => {
+        e.preventDefault();
+        const form = e.target;
+        const pdfLink = form.pdfLink.value;
+        const userComment = form.userComment.value;
+        const postedEmail = user?.email;
+        const formInfo = { pdfLink, userComment, postedEmail}
+        if (pdfLink && userComment) {
+            axios.post('http://localhost:5000/userPost', formInfo)
+                .then(() => {
+                    Swal.fire({
+                        title: "Good job!",
+                        text: "Submission assignment posted done!",
+                        icon: "success"
+                    });
+                    form.reset();
+                })
+                .catch((error) => { console.log(error); });
+        }
+        console.log(formInfo);
+    }
 
     return (
         <>
@@ -21,7 +47,6 @@ const AssignmentDetails = () => {
                 <title>Assignment Details || Group Study</title>
             </Helmet>
             <PageTitle title={'Assignments Details'} />
-
             <div className="">
                 <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
                     <div className="grid gap-10 lg:grid-cols-2">
@@ -81,7 +106,7 @@ const AssignmentDetails = () => {
             {/* this is modal area */}
             <div onClick={() => setOpenModal(false)} className={`fixed flex justify-center items-center z-[100] ${openModal ? 'visible opacity-1' : 'invisible opacity-0'} inset-0 w-full h-full backdrop-blur-sm bg-black/20 duration-100`}>
                 <div onClick={(e_) => e_.stopPropagation()} className={`absolute w-full lg:w-[500px] bg-white drop-shadow-2xl rounded-lg ${openModal ? 'opacity-1 duration-300 translate-y-0' : '-translate-y-20 opacity-0 duration-150'}`}>
-                    <form className="p-1 md:p-12">
+                    <form onSubmit={handleSubmitAssignment} className="p-1 md:p-12">
                         <svg onClick={() => setOpenModal(false)} className="w-10 mx-auto mr-0 cursor-pointer" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M6.99486 7.00636C6.60433 7.39689 6.60433 8.03005 6.99486 8.42058L10.58 12.0057L6.99486 15.5909C6.60433 15.9814 6.60433 16.6146 6.99486 17.0051C7.38538 17.3956 8.01855 17.3956 8.40907 17.0051L11.9942 13.4199L15.5794 17.0051C15.9699 17.3956 16.6031 17.3956 16.9936 17.0051C17.3841 16.6146 17.3841 15.9814 16.9936 15.5909L13.4084 12.0057L16.9936 8.42059C17.3841 8.03007 17.3841 7.3969 16.9936 7.00638C16.603 6.61585 15.9699 6.61585 15.5794 7.00638L11.9942 10.5915L8.40907 7.00636C8.01855 6.61584 7.38538 6.61584 6.99486 7.00636Z" fill="#000000"></path></g></svg>
                         <h1 className="backdrop-blur-sm text-xl md:text-4xl pb-8">
                             Assignment submission form
@@ -100,7 +125,7 @@ const AssignmentDetails = () => {
                             </div>
                         </div>
                         {/* button type will be submit for handling form submission*/}
-                        <button type="button" className="w-full py-2 px-5 mb-4 mt-6 shadow-lg rounded-lg before:block before:-left-1 before:-top-1 before:bg-black before:rounded-lg before:absolute before:h-0 before:w-0 before:hover:w-[100%] before:hover:h-[100%]  before:duration-500 before:-z-40 after:block after:-right-1 after:-bottom-1 after:bg-black after:rounded-lg after:absolute after:h-0 after:w-0 after:hover:w-[100%] after:hover:h-[100%] after:duration-500 after:-z-40 bg-white relative inline-block">Submit</button>
+                        <button type="submit" className="w-full py-2 px-5 mb-4 mt-6 shadow-lg rounded-lg before:block before:-left-1 before:-top-1 before:bg-black before:rounded-lg before:absolute before:h-0 before:w-0 before:hover:w-[100%] before:hover:h-[100%]  before:duration-500 before:-z-40 after:block after:-right-1 after:-bottom-1 after:bg-black after:rounded-lg after:absolute after:h-0 after:w-0 after:hover:w-[100%] after:hover:h-[100%] after:duration-500 after:-z-40 bg-white relative inline-block">Submit</button>
                     </form>
                 </div>
             </div>
